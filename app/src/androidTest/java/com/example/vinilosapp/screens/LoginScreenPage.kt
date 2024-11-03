2c49d3390fc6b7e3c3389d58805f4ccb43a2d9f1
Martin Galvan <md.galvan@uniandes.edu.co>
2024-11-03 17:14:01 -0500

    E2E test setup (#64)
    * Update README.md
    
    * updated build file for lint
    
    * updated back url
    
    * setup completed

--- app/src/androidTest/java/com/example/vinilosapp/screens/LoginScreenPage.kt
+++ app/src/androidTest/java/com/example/vinilosapp/screens/LoginScreenPage.kt
@@ -13,8 +13,8 @@ class LoginScreenPage@Inject constructor(
     private val composeRule = composeRuleHolder.composeRule
 
     private val appLogoTag = "AppLogo"
-    private val loginPromptTag = "LoginPrompt"
     private val invitadoButtonTag = "InvitadoButton"
+
 
     /**
      * Asserts that the app logo is displayed.
@@ -22,6 +22,7 @@ class LoginScreenPage@Inject constructor(
     fun assertAppLogoIsDisplayed() {
         composeRule.onNodeWithTag(appLogoTag).assertIsDisplayed()
     }
+
 
     /**
      * Clicks the "Invitado" button to login as a guest.
