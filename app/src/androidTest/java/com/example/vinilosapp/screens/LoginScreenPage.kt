22e76f4c0f974e3cc24eb37dc966b6c2c45c7e33
Martin Galvan <md.galvan@uniandes.edu.co>
2024-11-03 17:14:01 -0500

    E2E test setup (#64)
    * Update README.md
    
    * updated build file for lint
    
    * updated back url
    
    * setup completed

--- app/src/androidTest/java/com/example/vinilosapp/screens/LoginScreenPage.kt
+++ app/src/androidTest/java/com/example/vinilosapp/screens/LoginScreenPage.kt
@@ -1,20 +1,25 @@
 package com.example.vinilosapp.screens
 
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.AndroidComposeTestRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
-import com.example.vinilosapp.steps.ComposeRuleHolder
-import javax.inject.Inject
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.example.vinilosapp.MainActivity
 
-class LoginScreenPage@Inject constructor(
-    private val composeRuleHolder: ComposeRuleHolder,
-) {
+class LoginScreenPage(val composeRule: AndroidComposeTestRule<ActivityScenarioRule<MainActivity>, MainActivity>) {
 
-    private val composeRule = composeRuleHolder.composeRule
-
+    // Test tags
     private val appLogoTag = "AppLogo"
+    private val loginPromptTag = "LoginPrompt"
     private val invitadoButtonTag = "InvitadoButton"
 
+    /**
+     * Asserts that the login prompt text is displayed.
+     */
+    fun assertLoginPromptIsDisplayed() {
+        composeRule.onNodeWithTag(loginPromptTag).assertIsDisplayed()
+    }
 
     /**
      * Asserts that the app logo is displayed.
@@ -23,6 +28,9 @@ class LoginScreenPage@Inject constructor(
         composeRule.onNodeWithTag(appLogoTag).assertIsDisplayed()
     }
 
+    fun assertInvitadoButtonIsDisplayed() {
+        composeRule.onNodeWithTag(appLogoTag).assertIsDisplayed()
+    }
 
     /**
      * Clicks the "Invitado" button to login as a guest.
