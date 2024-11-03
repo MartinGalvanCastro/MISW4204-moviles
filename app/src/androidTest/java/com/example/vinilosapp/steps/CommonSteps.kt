86989298bf0106ce90a9b415b7501da5ee3951e4
Martin Galvan <md.galvan@uniandes.edu.co>
2024-11-03 17:14:01 -0500

    E2E test setup (#64)
    * Update README.md
    
    * updated build file for lint
    
    * updated back url
    
    * setup completed

--- app/src/androidTest/java/com/example/vinilosapp/steps/CommonSteps.kt
+++ app/src/androidTest/java/com/example/vinilosapp/steps/CommonSteps.kt
@@ -1,33 +1,25 @@
 package com.example.vinilosapp.steps
 
-import com.example.vinilosapp.screens.LoginScreenPage
+import android.util.Log
 import dagger.hilt.android.testing.HiltAndroidTest
 import io.cucumber.java.After
 import io.cucumber.java.Before
-import io.cucumber.java.en.When
 import javax.inject.Inject
 
 @HiltAndroidTest
 class CommonSteps @Inject constructor(
     private val composeRuleHolder: ComposeRuleHolder,
-    private val loginScreenPage: LoginScreenPage,
 ) {
 
     @Before(order = 1)
     fun setupComposeTestRule() {
+        Log.d("CommonSteps", "Setting up ComposeTestRule")
         composeRuleHolder.composeRule.waitForIdle()
     }
 
     @After(order = 1)
     fun teardownComposeTestRule() {
-        composeRuleHolder.composeRule.runOnUiThread {
-            composeRuleHolder.composeRule.activity.recreate()
-        }
-    }
-
-    @When("Un usuario invitado ingresa a Vinilos App")
-    fun invitadoIngresaAVinilosApp() {
-        loginScreenPage.assertAppLogoIsDisplayed()
-        loginScreenPage.clickInvitadoButton()
+        Log.d("CommonSteps", "Tearing down ComposeTestRule")
+        // Any necessary cleanup logic here
     }
 }
