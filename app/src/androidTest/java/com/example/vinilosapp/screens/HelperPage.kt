aa8f4b1b40a14eb0a92ea948e2145f4ad1600fd9
Martin Galvan <md.galvan@uniandes.edu.co>
2024-11-03 19:31:42 -0500

    bug fixes en test e2e
    Co-authored-by: Diego Fonseca <da.fonseca@uniandes.edu.co>
    Co-authored-by: Juan Lopez  <ja.lopez123@uniandes.edu.co>
    Co-authored-by: Andres Duque <a.duquec@uniandes.edu.co>

--- app/src/androidTest/java/com/example/vinilosapp/screens/HelperPage.kt
+++ app/src/androidTest/java/com/example/vinilosapp/screens/HelperPage.kt
@@ -1,36 +1,13 @@
 package com.example.vinilosapp.screens
 
-import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.performTextInput
-import com.example.vinilosapp.steps.ComposeRuleHolder
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.UiDevice
 import javax.inject.Inject
 
-class HelperPage @Inject constructor(
-    private val composeRuleHolder: ComposeRuleHolder,
-) {
+class HelperPage @Inject constructor() {
 
-    private fun clickNavbarItem(testTag: String) {
-        composeRuleHolder.composeRule.onNodeWithTag(testTag).performClick()
-    }
-
-    fun enterFilterText(text: String, filterTag: String = "filterTextField") {
-        composeRuleHolder.composeRule.onNodeWithTag(filterTag).performTextInput(text)
-    }
-
-    fun navigateToAlbumes() {
-        clickNavbarItem("navbar-item-Albumes")
-    }
-
-    fun navigateToArtistas() {
-        clickNavbarItem("navbar-item-Artistas")
-    }
-
-    fun navigateToBandas() {
-        clickNavbarItem("navbar-item-Bandas")
-    }
-
-    fun navigateToColeccionistas() {
-        clickNavbarItem("navbar-item-Coleccionistas")
+    fun hideKeyboard() {
+        val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+        device.pressBack()
     }
 }
