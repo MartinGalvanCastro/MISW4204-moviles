f1839115d6e3bc26301c9e6dec9231b57e402bd0
Martin Galvan <md.galvan@uniandes.edu.co>
2024-11-16 21:59:31 -0500

    Feat/micro optimizaciones (#72)
    * HU05 Implementada
    
    Co-authored-by: Diego Fonseca <da.fonseca@uniandes.edu.co>
    Co-authored-by: Juan Lopez <ja.lopez123@uniandes.edu.co>
    Co-authored-by: Andres Duque <a.duquec@uniandes.edu.co>
    
    * performance i/o
    
    * performance boost
    
    * performance i/o
    
    * performance boost
    
    ---------
    
    Co-authored-by: Diego Fonseca <da.fonseca@uniandes.edu.co>
    Co-authored-by: Juan Lopez <ja.lopez123@uniandes.edu.co>
    Co-authored-by: Andres Duque <a.duquec@uniandes.edu.co>

--- app/src/main/java/com/example/vinilosapp/repository/MusicianRepository.kt
+++ app/src/main/java/com/example/vinilosapp/repository/MusicianRepository.kt
@@ -2,6 +2,7 @@ package com.example.vinilosapp.repository
 
 import com.example.models.MusicianDetailDTO
 import com.example.models.MusicianSimpleDTO
+import com.example.vinilosapp.di.Cache
 import com.example.vinilosapp.services.adapters.MusicianServiceAdapter
 import com.example.vinilosapp.utils.NetworkChecker
 import javax.inject.Inject
@@ -9,7 +10,13 @@ import javax.inject.Inject
 class MusicianRepository @Inject constructor(
     private val musicianServiceAdapter: MusicianServiceAdapter,
     networkChecker: NetworkChecker,
-) : BaseRepository<MusicianSimpleDTO, MusicianDetailDTO>(networkChecker) {
+    cache: Cache,
+) : BaseRepository<MusicianSimpleDTO, MusicianDetailDTO>(
+    cache,
+    networkChecker,
+    MusicianSimpleDTO::class.java,
+    MusicianDetailDTO::class.java,
+) {
 
     override suspend fun fetchAllItems(): Result<List<MusicianSimpleDTO>> {
         return musicianServiceAdapter.getMusicians()
